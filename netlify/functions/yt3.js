94af08cfa6a30198b1fc95f63e7316ec43692788
yousef <amreky205@gmail.com>
2026-03-12 18:09:28 -0700

    Update yt3.js

--- netlify/functions/yt3.js
+++ netlify/functions/yt3.js
@@ -1,1 +1,19 @@
+export default async function handler(req, res) {
 
+const { url } = req.query
+
+if (!url) {
+return res.status(400).json({
+success: false,
+message: "حط رابط يوتيوب"
+})
+}
+
+const api = `https://obito-mr-apis.vercel.app/api/download/youtube?url=${encodeURIComponent(url)}`
+
+const response = await fetch(api)
+const data = await response.json()
+
+res.status(200).json(data)
+
+}
