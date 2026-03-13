4a9f76316e2319f95f24c615d0cecb1fec482e81
yousef <amreky205@gmail.com>
2026-03-12 17:50:27 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,71 @@
+/* ????? ?????? */
+
+let user = localStorage.getItem("user");
+
+if(!user){
+window.location.href="index.html";
+}
+
+/* ????? ??????? */
+
+let email = localStorage.getItem("email") || "user@email.com";
+
+document.getElementById("userEmail").innerText = email;
+
+/* ???? ????????? */
+
+let profileImage = localStorage.getItem("profileImage");
+
+if(profileImage){
+document.getElementById("profileImage").src = profileImage;
+}
+
+/* ????? API KEY */
+
+let key = localStorage.getItem("apikey");
+
+if(!key){
+
+key = "sk_" + Math.random().toString(36).substring(2,12) + "_" + Date.now();
+
+localStorage.setItem("apikey",key);
+
+}
+
+document.getElementById("apikey").innerText = key;
+
+/* ??? API */
+
+function copyAPI(){
+
+navigator.clipboard.writeText(key);
+
+alert("API Key Copied ?");
+
+}
+
+/* ?????? */
+
+function goDashboard(){
+window.location.href="dashboard.html";
+}
+
+function goApis(){
+window.location.href="apis.html";
+}
+
+/* ??????? */
+
+function contact(){
+window.open("https://t.me/Songmd9");
+}
+
+/* ????? ?????? */
+
+function logout(){
+
+localStorage.clear();
+
+window.location.href="index.html";
+
+}
---
+++ yt3.js
@@ -0,0 +1,40 @@
+export async function handler(event) {
+
+const params = event.queryStringParameters
+const url = params.url
+
+if (!url) {
+return {
+statusCode: 400,
+body: JSON.stringify({
+success: false,
+message: "ضع رابط يوتيوب"
+})
+}
+}
+
+try {
+
+const api = `https://obito-mr-apis.vercel.app/api/download/youtube?url=${encodeURIComponent(url)}`
+
+const res = await fetch(api)
+const data = await res.json()
+
+return {
+statusCode: 200,
+body: JSON.stringify(data)
+}
+
+} catch (err) {
+
+return {
+statusCode: 500,
+body: JSON.stringify({
+success: false,
+error: err.message
+})
+}
+
+}
+
+}
