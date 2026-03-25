80ff0ac6fd8ba61da1d98303780959e10a6ebeb4
yousef <amreky205@gmail.com>
2026-03-25 15:21:56 -0700

    Update gpt.js

--- api/gpt.js
+++ api/gpt.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const api = `https://obito-mr-apis.vercel.app/api/ai/writecream?prompt=${encodeURIComponent("JESSICA شخصية مصرية لطيفة ورومانسية")}&txt=${encodeURIComponent(q)}`;
+    const api = `https://www.emam-api.web.id/home/sections/Ai/api/Gpt/turbo?q=${encodeURIComponent(q)}`;
 
     const response = await fetch(api);
     const data = await response.json();
@@ -17,7 +17,8 @@ export default async function handler(req, res) {
     return res.status(200).json({
       success: true,
       question: q,
-      reply: data.reply || "❌ مفيش رد"
+      reply: data.message || "❌ مفيش رد",
+      author: "TOJI" // 👈 غيرناها هنا
     });
 
   } catch (err) {
